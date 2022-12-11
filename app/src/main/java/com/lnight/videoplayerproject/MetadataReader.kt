eb3871f2accab84f07ba786e323d23205e0d15ac
Lnight <94696816+EvgenyPlaksin@users.noreply.github.com>
2022-12-11 12:54:36 +0100

    Initial commit

---
+++ app/src/main/java/com/lnight/videoplayerproject/MetadataReader.kt
@@ -0,0 +1,42 @@
+package com.lnight.videoplayerproject
+
+import android.app.Application
+import android.net.Uri
+import android.provider.MediaStore
+
+data class Metadata(
+    val fileName: String
+)
+
+interface MetadataReader {
+    fun getMetadataFromUri(contentUri: Uri): Metadata?
+}
+
+class MetadataReaderImpl(
+    private val app: Application
+) : MetadataReader {
+
+    override fun getMetadataFromUri(contentUri: Uri): Metadata? {
+        if(contentUri.scheme != "content") {
+            return null
+        }
+        val fileName = app.contentResolver
+            .query(
+                contentUri,
+                arrayOf(MediaStore.Video.VideoColumns.DISPLAY_NAME),
+                null,
+                null,
+                null
+            )
+            ?.use { cursor ->
+                val index = cursor.getColumnIndex(MediaStore.Video.VideoColumns.DISPLAY_NAME)
+                cursor.moveToFirst()
+                cursor.getString(index)
+            }
+        return fileName?.let { fullFileName ->
+            Metadata(
+                fileName = Uri.parse(fullFileName).lastPathSegment ?: return null
+            )
+        }
+    }
+}
